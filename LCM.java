7e48162b6e88a987e4bdb4e9011dd9c8359347f4
BhagyasRii <119729822+BhagyasRii@users.noreply.github.com>
2023-08-14 00:28:57 +0530

    import java.util.Scanner; public class A{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int n = sc.nextInt();         int m = sc.nextInt();         int gcd=0;         for(int i=1;i<=Math.max(n,m);i++){             if(n%i==0 && m%i==0){                 gcd=i;             }         }         System.out.println((n*m)/gcd);     } }

---
+++ LCM.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class A{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int gcd=0;
+        for(int i=1;i<=Math.max(n,m);i++){
+            if(n%i==0 && m%i==0){
+                gcd=i;
+            }
+        }
+        System.out.println((n*m)/gcd);
+    }   
+}
