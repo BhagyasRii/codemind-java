4f99fb19cf0846502ec33b6903cedb044cb51526
BhagyasRii <119729822+BhagyasRii@users.noreply.github.com>
2023-08-14 01:41:56 +0530

    import java.util.Scanner; public class A{     public static void main(String[] args){       Scanner sc = new Scanner(System.in);       int l = sc.nextInt();       double c = l*(9/5.0)+(32);    System.out.printf("%.2f",c);     } }

---
+++ Celsius_To_Fahrenheit.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class A{
+    public static void main(String[] args){
+      Scanner sc = new Scanner(System.in);
+      int l = sc.nextInt();
+      double c = l*(9/5.0)+(32);
+   System.out.printf("%.2f",c);
+    }
+}
