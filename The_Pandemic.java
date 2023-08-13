df2df8b256967644d9f2b8bbdf5d22e783e86fa8
BhagyasRii <119729822+BhagyasRii@users.noreply.github.com>
2023-08-13 17:53:10 +0530

    import java.util.Scanner; public class A{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int x = sc.nextInt();         if(x<3)         System.out.println(x-1);         else         System.out.println("2");
        }
    }

---
+++ The_Pandemic.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class A{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        if(x<3)
+        System.out.println(x-1);
+        else
+        System.out.println("2");
+        
+        
+    }
+}
