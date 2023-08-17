3b35372645519667ae115f72f5774ab2f88aa0cc
BhagyasRii <119729822+BhagyasRii@users.noreply.github.com>
2023-08-17 12:04:43 +0530

    import java.util.Scanner; public class A{     public static void main(String [] args){         Scanner sc = new Scanner(System.in);         int a = sc.nextInt();         int b = sc.nextInt();         int c = sc.nextInt();         int maxi = Math.max(Math.max(a,b),c);         System.out.print(maxi);     } }

---
+++ Largest_number_among_3_numbers.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class A{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int maxi = Math.max(Math.max(a,b),c);
+        System.out.print(maxi);
+    }
+}
