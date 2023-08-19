de6d2d75d209fa802bae5435eb6f046f49568f18
BhagyasRii <119729822+BhagyasRii@users.noreply.github.com>
2023-08-19 10:07:34 +0530

    import java.util.Scanner; public class A{     public static int prime(int x){         int c=0;         if(x==1){             return -1;         }         for(int i=2;i<=Math.sqrt(x);i++){             if(x%i==0){                 return -1;             }         }         return 1;     }     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int n1 = sc.nextInt();         int d = (int)Math.log10(n1)+1;         int c=0;         if(prime(n1)==1)        {            while(n1!=0){                int r=n1%10;                if(prime(r)==1) c++;                n1=n1/10;            }            if(c==d){                System.out.println("Mega Prime");            }            else{                System.out.println("Not Mega Prime");            }        }        else{            System.out.println("Not Mega Prime");        }     } }

--- mega_prime.java
+++ mega_prime.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class A{
     public static int prime(int x){
+        int c=0;
         if(x==1){
             return -1;
         }
@@ -14,25 +15,24 @@ public class A{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n1 = sc.nextInt();
-        int d=(int) Math.log10(n1)+1;
+        int d = (int)Math.log10(n1)+1;
         int c=0;
-        if(prime(n1)==1){
-            while(n1!=0){
-                int r=n1%10;
-                if(prime(r)==1){
-                    c++;
-                }
-                n1=n1/10;
-            }
-            if(c==d){
-                System.out.println("Mega Prime");
-            }
-            else{
-                System.out.println("Not Mega Prime");
-            }
-        }
-        else{
-            System.out.println("Not Mega Prime");
-        }
+        if(prime(n1)==1)
+       {
+           while(n1!=0){
+               int r=n1%10;
+               if(prime(r)==1) c++;
+               n1=n1/10;
+           }
+           if(c==d){
+               System.out.println("Mega Prime");
+           }
+           else{
+               System.out.println("Not Mega Prime");
+           }
+       }
+       else{
+           System.out.println("Not Mega Prime");
+       }
     }
 }
